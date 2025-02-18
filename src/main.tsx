66a318266621a8918fa40ac1c4e631e283b43d9a
Andrew Cho <cho.yoonho023@gmail.com>
2025-02-18 11:22:07 -0500

    Initial ready

--- src/main.tsx
+++ src/main.tsx
@@ -7,3 +7,11 @@ createRoot(document.getElementById('root')!).render(
     <App />
   </StrictMode>,
 )
+
+/*
+1. Create the component
+2. Start off with interface
+3. edit the App.tsx for the import
+4. edit type.ts for the basic setup of HOW THE FUCK it looks.
+5. change the component using useState and useEffect.
+*  */
