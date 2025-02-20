341e701fcab5bb03484ff7281e8c08db405c3d8c
Andrew Cho <cho.yoonho023@gmail.com>
2025-02-19 23:25:23 -0500

    Error fixed

--- src/interfaces/Weather.ts
+++ src/interfaces/Weather.ts
@@ -1,3 +1,11 @@
 export interface Weather {
-
+    city: string;
+    country: string;
+    time: string;
+    name?: string;
+    region?: string;
+    cloud: number;
+    humidity: number;
+    wind: number;
+    icon: string;
 }
